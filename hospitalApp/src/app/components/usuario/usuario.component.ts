6500454797ebd3745e012554e67e04d313055606
Daniel Santos <danielgdt88@hotmail.com>
2024-11-19 21:14:19 -0300

    feat: crio component de button pra consulta e aplico estilos

--- hospitalApp/src/app/components/usuario/usuario.component.ts
+++ hospitalApp/src/app/components/usuario/usuario.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "./header/header.component";
 import {ConsultasComponent} from "./consultas/consultas.component";
+import {AgendarConsultaComponent} from "./agendar-consulta/agendar-consulta.component";
 
 @Component({
   selector: 'app-usuario',
   standalone: true,
   imports: [
     HeaderComponent,
-    ConsultasComponent
+    ConsultasComponent,
+    AgendarConsultaComponent
   ],
   templateUrl: './usuario.component.html',
   styleUrl: './usuario.component.css'
