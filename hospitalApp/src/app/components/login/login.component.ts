671e0e96e35486db34d1ffc1e1213dd58cff2c3d
Leticia <leticiammlima.contato@gmail.com>
2024-11-29 20:18:34 -0300

    IMPLEMENTAR REQUISIÇÃO POST PARA LOGIN

--- hospitalApp/src/app/components/login/login.component.ts
+++ hospitalApp/src/app/components/login/login.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginDados = {
+    email: '',
+    senha: ''
+  };
 
+  constructor(private http: HttpClient) {}
+
+  fazerLogin() {
+    const url = '#'; //adicionar url da api
+    this.http.post<any>(url, this.loginDados).subscribe({
+      next: (response: any) => {
+        console.log('Login realizado com sucesso!', response);
+        alert('Login realizado com sucesso!');
+      },
+      error: (error: any) => {
+        console.error('Erro ao fazer login:', error);
+        alert('Erro ao realizar o login.');
+      }
+    });
+  }
 }
