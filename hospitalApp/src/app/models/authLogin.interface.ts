2da3ad63924534cf09a17938041cfc27a2e72a58
Daniel Santos <danielgdt88@hotmail.com>
2024-11-30 20:50:59 -0300

    feat: conserto tipagem do retorno do login

--- hospitalApp/src/app/models/authLogin.interface.ts
+++ hospitalApp/src/app/models/authLogin.interface.ts
@@ -2,10 +2,13 @@ import { ERole } from './role.enum';
 
 export interface IAuthLogin {
   user: {
+    id: string;
     name: string;
     email: string;
     password: null;
     role: ERole;
+    createdAt: string;
+    updateAt: string;
   };
   token: string;
 }
