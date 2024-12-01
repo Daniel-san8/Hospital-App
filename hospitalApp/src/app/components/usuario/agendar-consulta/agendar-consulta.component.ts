f261dbf98c8fe1e07de143fed8638907d9bd76ae
nessamisiti <vanessapereiraalmeida29@gmail.com>
2024-12-01 15:13:17 -0300

    form post agendamento de consulta

--- hospitalApp/src/app/components/usuario/agendar-consulta/agendar-consulta.component.ts
+++ hospitalApp/src/app/components/usuario/agendar-consulta/agendar-consulta.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
-
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ReqHttpService } from '../../../services/req-http.service';
+import { NgClass } from '@angular/common';
+import { IPostConsulta } from '../../../models/postConsulta.interface';
 @Component({
   selector: 'app-agendar-consulta',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, NgClass],
   templateUrl: './agendar-consulta.component.html',
-  styleUrl: './agendar-consulta.component.css'
+  styleUrls: ['./agendar-consulta.component.css', '/src/styles.css'],
 })
 export class AgendarConsultaComponent {
+  form = new FormGroup({
+    specialty: new FormControl(null, [Validators.required]),
+    doctor: new FormControl(null, [Validators.required]),
+    date: new FormControl(null, [Validators.required]),
+    time: new FormControl(null, [Validators.required]),
+    obs: new FormControl(null, [Validators.required]),
+  });
 
+  constructor(private reqHttp: ReqHttpService, private router: Router) {}
+  agendarConsulta() {
+    const paylod: IPostConsulta = this.form.value;
+    const authToken = this.getAuthToken();
+    const headers = authToken ? new HttpHeaders().set('Authorization', `Bearer ${authToken}`) : undefined;
+    
+    this.reqHttp.postAgendarConsulta({ ...paylod }, { headers }).subscribe({
+      next: () => this.router.navigate(['/registerAdmin']),
+      error: (err: any) => console.log('isso é um erro ' + JSON.stringify(err)),
+    });
+  }
+
+  private getAuthToken(): string | null {
+    const name = 'authToken=';
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+  
+    for (let i = 0; i < cookieArray.length; i++) {
+      let cookie = cookieArray[i].trim();
+      if (cookie.indexOf(name) === 0) {
+        return cookie.substring(name.length, cookie.length);
+      }
+    }
+    return null;
+  }
 }
