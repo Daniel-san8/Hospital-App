4cec0d43b80a7ea74adb83ed6bb132cb0224c0e6
Daniel San <danielgdt88@hotmail.com>
2024-12-04 17:53:51 -0300

    feat: guard para usuario

--- hospitalApp/src/app/guards/usuario.guard.ts
+++ hospitalApp/src/app/guards/usuario.guard.ts
@@ -1,12 +1,14 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, createUrlTreeFromSnapshot } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { ERole } from '../models/role.enum';
 
 export const usuarioGuard: CanActivateFn = (route) => {
   const cookies = inject(CookieService);
   const token = cookies.get('token');
+  const role = cookies.get('role');
 
-  if (!token) {
+  if (!token || role !== ERole.USER) {
     return createUrlTreeFromSnapshot(route, ['/login']);
   }
   return true;
